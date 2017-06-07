51ceabc3f8805fbdb3c728b5688208e36640c3fe
Mat Leonard <you@example.com>
2017-06-06 21:29:30 -0700

    Add function to plot images with predictions

--- intro-to-pytorch/helper.py
+++ intro-to-pytorch/helper.py
@@ -1,7 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from torch import nn, optim
 from torch.autograd import Variable
-
-import matplotlib.pyplot as plt
 
 def test_network(net, trainloader):
 
@@ -38,3 +38,18 @@ def view_recon(img, recon):
     for ax in axes:
         ax.axis('off')
         ax.set_adjustable('box-forced')
+        
+def view_classify(img, ps):
+    ''' Function for viewing an image and it's predicted classes.
+    '''
+    ps = ps.data.numpy().squeeze()
+
+    fig, (ax1, ax2) = plt.subplots(figsize=(6,7), ncols=2)
+    ax1.imshow(img.numpy().squeeze())
+    ax1.axis('off')
+    ax2.barh(np.arange(10), ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(np.arange(10))
+    ax2.set_yticklabels(np.arange(10).astype(int), size='large');
+    ax2.set_title('Digit Probability')
+    ax2.set_xlim(0, 1.1)
